8df2bbcf83952e14df31be662ff5d51db0171990
jmas <jmas.ukraine@gmail.com>
2022-01-29 21:31:56 +0200

    Update main

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ const addChannelsFilterListener = () => {
 
 const addFilterApplier = () => {
   document.getElementById("channels-data").addEventListener("change", () => {
-    setImmediate(() => {
+    setTimeout(() => {
       applyFilter();
     });
   });
